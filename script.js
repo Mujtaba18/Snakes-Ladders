16937a4ec34be9730a796b714406c4e3ccf60fda
Mujtaba18 <mujtab189@gmail.com>
2024-09-09 12:13:37 +0300

    Great Job Done, circle is moving well, snakes and ladders are functioning

--- script.js
+++ script.js
@@ -10,6 +10,27 @@ const diceButton = document.querySelector('#rollDice');
 
 let currentPosition = 9 * boardGrid;
 let circle = null;
+let isFirstMove = true;
+
+const snakes = {
+    'cell-0-3': 'cell-2-1',
+    'cell-0-5': 'cell-4-9',
+    'cell-1-7': 'cell-8-2',
+    'cell-3-1': 'cell-7-1',
+    'cell-4-3': 'cell-6-0',
+    'cell-4-8': 'cell-7-8',
+    'cell-8-3': 'cell-8-7'
+}
+
+const ladders = {
+    'cell-9-2': 'cell-7-0',
+    'cell-9-7': 'cell-7-9',
+    'cell-7-7': 'cell-1-3',
+    'cell-4-2': 'cell-2-3',
+    'cell-2-5': 'cell-1-5',
+    'cell-2-0': 'cell-0-0',
+    'cell-1-9': 'cell-0-9'
+}
 
 
 for (let row = 0; row < boardGrid; row++) {
@@ -45,8 +66,69 @@ function rollDice() {
 }
 
 function movePiece(moves) {
-    
+    let row = Math.floor(currentPosition / boardGrid);
+    let col = currentPosition % boardGrid;
+
+    if (isFirstMove) {
+        moves--;
+        isFirstMove = false;
+    }
+
+    for (let i = 0; i < moves; i++) {
+
+        if (row % 2 === 0) {
+            col--;
+            if (col < 0) {
+                col = 0;
+                row--;
+            }
+        } else {
+            col++;
+            if (col >= boardGrid) {
+                col = boardGrid - 1;
+                row--;
+            }
+        }
+        if (row < 0) {
+            row = 0;
+            col = 0;
+            break;
+        }
+    }
+
+    if (snakes[currentPosition]) {
+        currentPosition = snakes[currentPosition];
+    }
+    else if (ladders[currentPosition]) {
+        currentPosition = ladders[currentPosition];
+    }
+
+    if (document.querySelector("#cell-0-0").querySelector(".piece")) {
+        alert('Congratulations!!');
+    }
+
+    currentPosition = row * boardGrid + col;
+
+    const cellId = `cell-${row}-${col}`;
+    if (snakes[cellId]) {
+        currentPosition = parseCellId(snakes[cellId]);
+    } else if (ladders[cellId]) {
+        currentPosition = parseCellId(ladders[cellId]);
+    }
+
+    const newRow = Math.floor(currentPosition / boardGrid);
+    const newCol = currentPosition % boardGrid;
+    const newCell = document.querySelector(`#cell-${newRow}-${newCol}`);
+    if (newCell) {
+        newCell.appendChild(circle);
+    }
 }
+
+function parseCellId(cellId) {
+    const [row, col] = cellId.replace('cell-', '').split('-').map(Number);
+    return row * boardGrid + col;
+}
+
 
 
 diceButton.addEventListener('click', rollDice);
